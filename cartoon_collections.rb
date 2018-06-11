ea13d8811904bb30f7658f806da8525eb5260ddc
Paris Dolfman <parisdolfman@gmail.com>
2018-06-11 18:57:30 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,9 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(call)
+  veggies.collect do |call|
+  call.capitalize + "!"
 end
 
 def long_planeteer_calls# code an argument here
